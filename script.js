38dd0585288491222494adeceaf6a58028ba772c
Ayansaxena24 <ayan.saxena24@gmail.com>
2022-12-14 03:54:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let images = ["dice-01.svg",
+"dice-02.svg",
+"dice-03.svg",
+"dice-04.svg",
+"dice-05.svg",
+"dice-06.svg"];
+let dice = document.querySelectorAll("img");
+
+function roll(){
+    dice.forEach(function(die){
+        die.classList.add("shake");
+    });
+    setTimeout(function(){
+        dice.forEach(function(die){
+            die.classList.remove("shake");
+        });
+        let diceOneValue = Math.floor(Math.random()*6);
+        console.log(diceOneValue);
+        document.querySelector("#die-1").setAttribute("src", images[diceOneValue]);
+        document.querySelector("#total").innerHTML = "Your roll is " + (diceOneValue +1);
+    },
+    1000
+    );
+}
+roll();
